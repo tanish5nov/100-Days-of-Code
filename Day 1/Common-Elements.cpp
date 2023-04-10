6d2bed484749530207356242537f023ad4178a3e
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-04-10 20:59:23 +0530

    Create Common-Elements.cpp

---
+++ Day 1/Common-Elements.cpp
@@ -0,0 +1,19 @@
+//Back-end complete function Template for C++
+
+class Solution{
+    public:
+        vector<int> common_element(vector<int>v1,vector<int>v2)
+    {
+        vector<int>cmn;
+        sort(v1.begin(),v1.end());
+        sort(v2.begin(),v2.end());
+        int i=0, j=0;
+        while(i< v1.size() && j< v2.size()){
+            if(v1[i]==v2[j]){cmn.push_back(v1[i]);i++;j++;}
+            
+            else if(v1[i]>v2[j]){j++;}
+            else if(v1[i]<v2[j]){i++;}
+        }
+        return {cmn};
+    }
+};
