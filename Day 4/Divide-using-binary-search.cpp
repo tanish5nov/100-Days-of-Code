e195c476ba32caf6ecaa5ae41707d952f01cceca
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-04-13 18:07:07 +0530

    Update Divide-using-binary-search.cpp

--- Day 4/Divide-using-binary-search.cpp
+++ Day 4/Divide-using-binary-search.cpp
@@ -6,22 +6,24 @@ int solve(int dividend,int divisor){
   int s=0;
   int e=dividend-1;
   int mid=s+((e-s)/2);
-
+  int ans=1;
   while(s<=e){
     if(mid*divisor==dividend){
-      return mid;
+      ans=mid;
     }
     if(mid*divisor<dividend){
+      ans=mid;
       s=mid+1;
     }
     else{e=mid-1;}
     mid=s+((e-s)/2);
   }
+  return ans;
 }
 
 int main() {
   int divisor=3;
-  int dividend=101;
+  int dividend=100;
   int ans= solve(dividend,divisor);
   cout<<ans;
 }
