d0ea1a110618d7a0d52c4eb31342e6694973bc68
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-04-20 16:21:44 +0530

    Create reverse-vowels-of-a-string.cpp

---
+++ Day 11/reverse-vowels-of-a-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    bool isVowel(char ch){
+        ch= tolower(ch);
+        return ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u';
+    }
+    string reverseVowels(string s) {
+        int i=0;
+        int j=s.size()-1;
+
+        while(i<j){
+            if(isVowel(s[i]) && isVowel(s[j])){
+                swap(s[i],s[j]);
+                i++,j--;
+            }
+
+            else if(isVowel(s[i])==false){
+                i++;
+            }
+
+            else if(isVowel(s[j])==false){
+                j--;
+            }
+        }
+        return s;
+    }
+};
