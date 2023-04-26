277c36b7f3b09dd7fe659921882a0d163947a4d2
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-04-26 22:02:23 +0530

    Create Four-tickers.cpp

---
+++ DAy 17/Four-tickers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int x;
+	    cin>>x;
+	    
+	    if(x*4>1000){cout<<"NO"<<endl;}
+	    else{cout<<"YES"<<endl;}
+	}
+	return 0;
+}
