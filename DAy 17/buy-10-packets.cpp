e2dbdff1f7c6101a05e9b54eae120d2c9a1ae311
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-04-26 22:02:51 +0530

    Create buy-10-packets.cpp

---
+++ DAy 17/buy-10-packets.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int x,y;
+	    cin>>x;
+	    cin>>y;
+	    
+	    int mini= 2*y+x;
+	    cout<<mini<<endl;
+	}
+	return 0;
+}
