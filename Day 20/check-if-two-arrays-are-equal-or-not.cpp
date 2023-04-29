98c9822a76bb79878d847257055628fcc29d3304
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-04-29 12:56:37 +0530

    Create check-if-two-arrays-are-equal-or-not.cpp

---
+++ Day 20/check-if-two-arrays-are-equal-or-not.cpp
@@ -0,0 +1,19 @@
+//User function template for C++
+
+class Solution{
+    public:
+
+    //Function to check if two arrays are equal or not.
+    bool check(vector<ll> A, vector<ll> B, int N) {
+        sort(A.begin(),A.end());
+        sort(B.begin(),B.end());
+        
+        if(A.size()!=B.size()){return false;}
+        
+        for(int i=0; i<A.size(); i++){
+            if(A[i]!=B[i]){return false;}
+        }
+        
+        return true;
+    }
+};
