1eef3f150690b5f2bb4b9725a6fbe78081948d48
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-05-06 20:44:22 +0530

    Create print-subsequences.cpp

---
+++ Day 27/print-subsequences.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void printsubsequence(string str, string output, int i){
+  if(i>=str.length()){
+    cout<<output<<endl;
+    return;
+  }
+
+  //exclude
+  printsubsequence(str,output,i+1);
+
+  //include
+  output.push_back(str[i]);
+  printsubsequence(str,output,i+1);
+}
+
+int main() {
+  string str="abc";
+  string output="";
+  int i=0;
+  printsubsequence(str, output,i);
+  return 0;
+}
