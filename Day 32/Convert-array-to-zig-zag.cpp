a0a937ece919040b08e0e6af5e1189f2278cd2ec
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-05-11 20:58:42 +0530

    Create Convert-array-to-zig-zag.cpp

---
+++ Day 32/Convert-array-to-zig-zag.cpp
@@ -0,0 +1,15 @@
+// User function template for C++
+class Solution {
+  public:
+    // Program for zig-zag conversion of array
+    void zigZag(int arr[], int n) {
+        for(int i=0;i<n-1;i++){
+           if(i%2==0 && arr[i]>arr[i+1]){
+               swap(arr[i],arr[i+1]);
+           }
+           else if(i%2==1 && arr[i+1]>arr[i]){
+               swap(arr[i],arr[i+1]);
+           }
+        }
+    }
+};
