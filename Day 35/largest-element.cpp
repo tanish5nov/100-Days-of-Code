889d76daeb9556126148f53588f3e2a0baaae1a7
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-05-14 21:35:57 +0530

    Create largest-element.cpp

---
+++ Day 35/largest-element.cpp
@@ -0,0 +1,11 @@
+//User function Template for C++
+
+class Solution
+{
+public:
+    int largest(vector<int> &arr, int n)
+    {
+        sort(arr.begin(),arr.end());
+        return arr[n-1];
+    }
+};
