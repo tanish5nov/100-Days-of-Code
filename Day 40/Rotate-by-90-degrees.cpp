224a3a87bbc810323ecbfc1847cd89f5d6060783
Tanish Sharma <109678180+geekytanish@users.noreply.github.com>
2023-05-19 18:05:12 +0530

    Create Rotate-by-90-degrees.cpp

---
+++ Day 40/Rotate-by-90-degrees.cpp
@@ -0,0 +1,16 @@
+
+class Solution
+{   
+    public:
+    //Function to rotate matrix anticlockwise by 90 degrees.
+    void rotateby90(vector<vector<int> >& matrix, int n) 
+    { 
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        //reverse 
+      reverse(matrix.begin(),matrix.end());
+    } 
+};
